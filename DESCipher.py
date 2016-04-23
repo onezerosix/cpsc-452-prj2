0eec1755e530331730659c0978dbfa767e477f85
EricJR <ericjohnroe@gmail.com>
2016-04-23 14:24:26 -0700

    Updated DESCipher.py (16 char input)
    Allows DESCipher.py work with 16 character hexidecimal input (as Gofman requested in the project).

--- DESCipher.py
+++ DESCipher.py
@@ -4,6 +4,7 @@ Assignment 2
 DES Cipher Class
 
 Programmer: Alejandro Sanchez
+Modified 4/23/16: Eric Roe
 Python 2.7
 
 Required Files: Cipherinterface.py
@@ -13,57 +14,55 @@ encryption and decryption with the DES class from the pyCrpto library.
 
 
 
-To install pyCrpto on linux/Ubuntu :
-
-$pip install pycrypto
+To install pyCrypto on linux/Ubuntu :
+  $pip install pycrypto
 """
 
 from Crypto.Cipher import DES
-from Crypto import Random 	#will potentially be used with IV for other Block Cipher modes
+from Crypto import Random 	                                # Will potentially be used with IV for other Block Cipher modes
 from CipherInterface import CipherInterface
 import string
 
 class DESCipher (CipherInterface):
-	__slots__ ={'_key'}
+  __slots__ ={'_key'}
   
-  #Default initialization
-	def __init__(self):
-		self._key = None
-    	self._padFlag = 0  #Keep track of whether or not plaintext was padded before encryption 0 = no, 1 = yes
+  # Default initialization
+  def __init__(self):
+    self._key = None
+    self._padFlag = 0                                       # Keep track of whether or not plaintext was padded before encryption; 0 = no, 1 = yes
 
-	def setKey(self,key):
-		if len(key) is 8 and all(character in string.hexdigits for character in key):  # If the key length is 16 (8 bytes) and contains only Hexadecimal characters
-			self._key = key                # Set the encryption/decryption key
-			return True                    # Return True, Key successfully set
-		else:								# Otherwise...
-			return False                    # The Key is invalid
-	def encrypt(self,plaintext):
-		DESkey = self._key   #set decryption key to key passed as parameter
-		enciphered = ""			# encrypted plaintext
-		cipher = DES.new(DESkey) # Create new DES object with given key, default mode is ECB mode
+  def setKey(self,key):
+    if len(key) is 16 and all(char in string.hexdigits for char in key):  # Check that the key is 16 hexidecimal characters
+      key = str(bytearray.fromhex(key))                     # Change the key to a byte array (Crypto library requires 8 byte input)
+      self._key = key                                       # Set the encryption/decryption key
+      return True                                           # key is valid and successfully set
+    else:
+      return False                                          # key is invalid and rejected
+
+  def encrypt(self,plaintext):
+    DESkey = self._key                                      # Set the key
+    ciphertext = ""			                                    # ciphertext will hold the encrypted plaintext
+    encipher = DES.new(DESkey)                              # Create new DES object with given key, which defaults to ECB mode
 		
-	# Check if plaintext perfectly fits into 64 bit blocks
-		if len(plaintext)% DES.block_size is not 0:	# if it doesn't	
-			self._padFlag = 1;						# then set the padFlag to 1, indicating that the plaintext is padded
-			padLength = DES.block_size - (len(plaintext) % DES.block_size) #generate padding for plaintext with bytes of 0's followed by the number of bytes added. i.e. 003 or 0004
-			for i in range(padLength -1):  # append padding of 0's to plaintext before encryption
-				plaintext += chr(0)
-			plaintext += chr(padLength)    #append number of bytes padded to end of plaintext
+    if len(plaintext) % DES.block_size is not 0:            # Check if the final plaintext block fits into 8 bytes (64 bits).
+      self._padFlag = 1;						                        # Set padFlag to 1, indicating that we are padding the plaintext.
 
-		enciphered = cipher.encrypt(plaintext) #create ciphertext
+      padLength = DES.block_size - (len(plaintext) % DES.block_size) # Calculate how many bytes must be padded
+                                                            # Padding format used: 0's followed by number of padded bytes.
+                                                            #   ex:   Last block of plaintext = "cat"
+                                                            #         Padded = "cat00005"
+      for i in range(padLength -1):                         # Append (padLength - 1) 0's to the plaintext.
+        plaintext += chr(0)
+      plaintext += chr(padLength)                           # Append number of bytes padded to end of plaintext (can hold values 1-7).
 
-		return enciphered
+    ciphertext = encipher.encrypt(plaintext)                # Encrypt the plaintext and store it in the ciphertext string
 
-	def decrypt(self,ciphertext): 
-		DESkey = self._key 								  #set decryption key to key passed as parameter
-		deciphered = ""									# decrypted ciphertext
-		cipher = DES.new(DESkey)						 # Create new DES object with given key, default mode is ECB mode
-		deciphered = cipher.decrypt(ciphertext)			 # decrpyt given ciphertext using DES object from Crypto library
-	# Check if plaintext was padded before ecnryption
-		#if self._padFlag is 1: 							# If plaintext was padded
-		#	lastIndex = deciphered[-1].encode("hex")	# obtain last nibble from decrypted ciphertext and convert to hex representation
-			#print ("last index in deciphered: ", int(lastIndex,16)) #Print decimal value of last index
-		#	deciphered = deciphered[:(len(deciphered) - int(lastIndex,16))] #Display plaintext without the padded bytes
-		#	self._padFlag= 0 							#reset _padFlag to 0
+    return ciphertext                                       # Return the ciphertext, which is our enciphered result
+
+  def decrypt(self,ciphertext): 
+    DESkey = self._key 								                      # Set the key
+    plaintext = ""									                        # plaintext will hold the decrypted ciphertext
+    decipher = DES.new(DESkey)						                  # Create new DES object with given key, which defaults to ECB mode
+    plaintext = decipher.decrypt(ciphertext)			          # Decrypt the ciphertext and store it in the plaintext string
 		
-		return deciphered
+    return plaintext                                        # Return the plaintext, which is our deciphered result
