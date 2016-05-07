da6dabe7842ca1b543167f9f1096eebc1b4704f4
EricJR <ericjohnroe@gmail.com>
2016-05-07 13:27:33 -0700

    Updated DESCipher.py 5/7/16 @ 1:26 PM
    Modified padding to be in a function, along with editing how padding works.

--- DESCipher.py
+++ DESCipher.py
@@ -46,19 +46,25 @@ class DESCipher (CipherInterface):
       success = False
     return success
 
+  # Padding format used: 0's followed by number of padded bytes.
+  #   ex:   Last block of plaintext = "cat"
+  #         Padded = "cat00005"
+  def pad(self,plaintext):
+    padLength = DES.block_size - (len(plaintext) % DES.block_size) # Calculate how many bytes must be padded
+
+    for i in range(padLength -1):                           # Append (padLength - 1) 0's to the plaintext.
+      plaintext += chr(ord('0'))                            # Add 0 (character) as a pad
+    plaintext += chr(ord(str(padLength)))                   # Append number of bytes padded to end of plaintext (can be anywhere in range 1-7).
+
+    return plaintext
+
   def encrypt(self,plaintext):
     DESkey = self._key                                      # Set the key
     ciphertext = ""			                                    # ciphertext will hold the encrypted plaintext
     encipher = DES.new(DESkey)                              # Create new DES object with given key, which defaults to ECB mode
 		
     if len(plaintext) % DES.block_size is not 0:            # Check if the final plaintext block fits into 8 bytes (64 bits).
-      padLength = DES.block_size - (len(plaintext) % DES.block_size) # Calculate how many bytes must be padded
-                                                            # Padding format used: 0's followed by number of padded bytes.
-                                                            #   ex:   Last block of plaintext = "cat"
-                                                            #         Padded = "cat00005"
-      for i in range(padLength -1):                         # Append (padLength - 1) 0's to the plaintext.
-        plaintext += chr(0)
-      plaintext += chr(padLength)                           # Append number of bytes padded to end of plaintext (can hold values 1-7).
+      plaintext = pad(plaintext)
 
     ciphertext = encipher.encrypt(plaintext)                # Encrypt the plaintext and store it in the ciphertext string
 
